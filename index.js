0850bc9c6d958686bc89bb33b39454ad51c03b7f
Kevin Guo <Kevin@Kevins-Air.home>
2019-09-07 09:24:03 -1000

    done

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function locate(array, target) {
   // do work here
+  let includesArray = array.join();
+
+  if (includesArray.includes(target)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = locate;
